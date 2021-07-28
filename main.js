ca75c9cd06a977e5c1095c58f0bbecd54f5c39cd
王春乔 <1356301996@qq.com>
2021-07-28 16:53:43 +0800

    '弹窗组件和视频时间跳转'

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import mydialog from './pages/components/dialog/index.vue';
 
 Vue.config.productionTip = false
+
+
+Vue.component('my-dialog',mydialog)
 
 App.mpType = 'app'
 
